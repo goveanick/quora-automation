727a5021612b0934b6afd4a9e622c6886939e79b
goveanick <goveanicolas@gmail.com>
2020-01-18 12:04:00 -0600

    commented out some stuff to see if test will pass in Jenkins

--- test/pageobjects/quora-login.js
+++ test/pageobjects/quora-login.js
@@ -21,52 +21,63 @@ class QuoraLogin {
     }
 
     enterCredentials(email, password) {
+        console.log('I\'m logging in');
         this.emailField.setValue(email);
         this.passwordField.setValue(password);
         this.submitButton.click();
+        console.log('I\'ve logged in');
     }
 
     clickOnFirstViewAllSuggestionsLink() {
+        console.log('I\'m on the partner homepage');
         browser.waitUntil(() => {
             return (this.requestAnswerBoxOnPartnerPage.isDisplayed());
         }, 30000, 'The box on the Partner\'s page that leads to the questions ' +
             'that needs more answers was not displayed' + browser.getUrl());
         this.viewAllSuggestions[0].click();
+        console.log('I\'ve clicked on the view all suggestions button on the partner homepage');
     }
 
     clickRequestButtons() {
+        console.log('I\'m on the page that displays the three questions');
         browser.waitUntil(() => {
             return (this.boxWith3QuestionsNeedingAnswers.isDisplayed());
         }, 30000, 'The box that displayed 3 questions needing answers was not displayed' + browser.getUrl());
-        console.log(this.allRequestButtons.length + ' This is where I think it Jenkins is at' );
+
+        console.log('I\'m entering the loop, that will click on the three questions');
+        let questionNumber = 1;
         for(const requestButton of this.allRequestButtons) {
             console.log('Im inside the loop');
-            // browser.waitUntil(() => {
-            //     return (requestButton.isClickable());
-            // }, 30000, 'iframe did not load correctly: ' + browser.getUrl());
+            browser.waitUntil(() => {
+                return (requestButton.isDisplayed());
+            }, 30000, 'The request button for question number '+ questionNumber + ' was not displayed.\n' + browser.getUrl());
             requestButton.click();
-            console.log('Im passed the click');
+            console.log('I\'ve clicked on question number ' + questionNumber);
             // browser.waitUntil(() => {
-            //     return (this.allAnswerButtons[0].isClickable());
+            //     return (this.allAnswerButtons[0].isDisplayed());
             // }, 30000, 'iframe did not load correctly: ' + browser.getUrl());
-            console.log('This is the number Im on: ' + requestButton);
-            console.log(this.allAnswerButtons.length);
-            let i = 0;
-            let maximumRequests = 5;
-            // if(this.allAnswerButtons.length < maximumRequests) {
-            //     maximumRequests = this.allAnswerButtons.length;
+            // console.log('This is the number Im on: ' + requestButton);
+            // console.log(this.allAnswerButtons.length);
+            // let i = 0;
+            // let maximumRequests = 5;
+            // // if(this.allAnswerButtons.length < maximumRequests) {
+            // //     maximumRequests = this.allAnswerButtons.length;
+            // // }
+            // while(i < maximumRequests){
+            //     browser.waitUntil(() => {
+            //         return (this.allAnswerButtons[i].isDisplayed());
+            //     }, 30000, 'iframe did not load correctly: ' + browser.getUrl());
+            //     this.allAnswerButtons[i].click();
+            //     i++;
             // }
-            while(i < maximumRequests){
-                // browser.waitUntil(() => {
-                //     return (this.allAnswerButtons[i].isClickable());
-                // }, 30000, 'iframe did not load correctly: ' + browser.getUrl());
-                this.allAnswerButtons[i].click();
-                i++;
-            }
-            // browser.waitUntil(() => {
-            //     return (this.popUpCloseButton.isClickable());
-            // }, 30000, 'iframe did not load correctly: ' + browser.getUrl());
-            this.popUpCloseButton.click()
+            browser.waitUntil(() => {
+                return (this.popUpCloseButton.isDisplayed());
+            }, 30000, 'The close button in the popup wasn\'t displayed' + browser.getUrl());
+            this.popUpCloseButton.click();
+            questionNumber++;
+            browser.waitUntil(() => {
+                return (this.allRequestButtons.length = 3);
+            }, 30000, 'The popup was not closed and the three questions were not displayed: ' + browser.getUrl());
         }
 
     }
--- test/specs/quora-request-answers.js
+++ test/specs/quora-request-answers.js
@@ -3,28 +3,18 @@ import QuoraLogin from '../pageobjects/quora-login';
 const yargs = require('yargs');
 const argv = yargs.argv;
 
-/*
-	This is a BDD test using Jasmine JavaScript framework
-*/
-
 describe('navigate to Quora and request answers', function() {
     it('should request answers of most popular questions ', function () {
-        contactUs.open();     // navigating to login page
+        contactUs.open();// navigating to login page
         QuoraLogin.enterCredentials(argv.userName, argv.passWord);
 
         let i = 0;
-        while(i < 50) {
+        while(i < /*50*/1) {
             QuoraLogin.clickOnFirstViewAllSuggestionsLink();
             QuoraLogin.clickRequestButtons();
-            QuoraLogin.clickRefreshButton();
+            // QuoraLogin.clickRefreshButton();
             contactUs.open();
             i++;
         }
-
-        // QuoraLogin.clickRequestAnswerButtons();
-        // browser.pause(10000);
-
-
-
     });
 });
