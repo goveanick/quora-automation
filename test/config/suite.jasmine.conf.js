1e04aea234c8a3a12e6971c45f92815fea99e63d
goveanick <goveanicolas@gmail.com>
2020-01-26 21:28:39 -0600

    enabled headless

--- test/config/suite.jasmine.conf.js
+++ test/config/suite.jasmine.conf.js
@@ -63,7 +63,7 @@ exports.config = {
         'goog:chromeOptions': {
           // to run chrome headless the following flags are required
           // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-          // args: ['--headless', '--disable-gpu', '--window-size=1600,1200', '--incognito'],
+          args: ['--headless', '--disable-gpu', '--window-size=1600,1200', '--incognito'],
         }
       },
 
