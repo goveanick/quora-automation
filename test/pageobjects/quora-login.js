851cfff8141c8aabb5f4f460e52ebee4088927c1
goveanick <goveanicolas@gmail.com>
2020-02-29 12:22:06 -0600

    added another if/else statement

--- test/pageobjects/quora-login.js
+++ test/pageobjects/quora-login.js
@@ -45,55 +45,63 @@ class QuoraLogin {
     clickRequestButtons3() {
         console.log('I\'m on the page that displays the three questions');
         console.log('The page with the three questions should be loaded now');
-        this.allRequestButtons[0].waitForDisplayed(30000);
-        this.allRequestButtons[1].waitForExist(30000);
-        this.allRequestButtons[1].waitForEnabled(30000);
-        this.allRequestButtons[1].click();
-        console.log('I\'ve clicked on the first question');
-        console.log('This is the value of the new method: ' + this.closeButtonPopUpVisible());
-        if (this.closeButtonPopUpVisible() && this.popUpCloseButton.isEnabled()) {
-            try {
-                this.popUpCloseButton.waitForDisplayed(30000);
-                this.popUpCloseButton.waitForExist(30000);
-                this.popUpCloseButton.waitForEnabled(30000);
-            }
-            catch(err) {
-                console.log('I fell into the catch, should click again');
-                contactUs.open();
-                this.clickOnFirstViewAllSuggestionsLink();
-                this.allRequestButtons[1].waitForDisplayed(30000);
-                this.allRequestButtons[1].waitForExist(30000);
-                this.allRequestButtons[1].waitForEnabled(30000);
-                this.allRequestButtons[1].click();
-                this.popUpCloseButton.waitForDisplayed(30000);
-                this.popUpCloseButton.waitForExist(30000);
-                this.popUpCloseButton.waitForEnabled(30000);
-                console.log('moving out of the catch');
-            }
-            finally {
-                console.log(this.allAnswerButtons.length);
-                if (this.allAnswerButtons.length > 5 && this.allAnswerButtons[0] !== undefined) {
-                    let i = 0;
-                    let maximumRequests = 5;
-                    while(i < maximumRequests){
-                        browser.waitUntil(() => {
-                            return (this.allAnswerButtons[i].isDisplayed());
-                        }, 20000, 'The individual request buttons were not clickable: ' + browser.getUrl());
-                        this.allAnswerButtons[i].click();
-                        i++;
-                    }
-                    console.log('Sent 5 requests');
-                } else {
-                    console.log('Sent 0 requests');
+        // this.allRequestButtons[0].waitForDisplayed(30000);
+        // this.allRequestButtons[1].waitForExist(30000);
+        // this.allRequestButtons[1].waitForEnabled(30000);
+        // this.allRequestButtons[1].click();
+        if (this.checkIfTheThreeQuestionsAreDisplayed()) {
+            this.allRequestButtons[1].click();
+            console.log('I\'ve clicked on the first question');
+            console.log('This is the value of the new method: ' + this.closeButtonPopUpVisible());
+            if (this.closeButtonPopUpVisible() && this.popUpCloseButton.isEnabled()) {
+                try {
+                    this.popUpCloseButton.waitForDisplayed(30000);
+                    this.popUpCloseButton.waitForExist(30000);
+                    this.popUpCloseButton.waitForEnabled(30000);
                 }
-                this.popUpCloseButton.click();
-                console.log('I\'ve clicked on the close button of the popup');
+                catch(err) {
+                    console.log('I fell into the catch, should click again');
+                    contactUs.open();
+                    this.clickOnFirstViewAllSuggestionsLink();
+                    this.allRequestButtons[1].waitForDisplayed(30000);
+                    this.allRequestButtons[1].waitForExist(30000);
+                    this.allRequestButtons[1].waitForEnabled(30000);
+                    this.allRequestButtons[1].click();
+                    this.popUpCloseButton.waitForDisplayed(30000);
+                    this.popUpCloseButton.waitForExist(30000);
+                    this.popUpCloseButton.waitForEnabled(30000);
+                    console.log('moving out of the catch');
+                }
+                finally {
+                    console.log(this.allAnswerButtons.length);
+                    if (this.allAnswerButtons.length > 5 && this.allAnswerButtons[0] !== undefined) {
+                        let i = 0;
+                        let maximumRequests = 5;
+                        while(i < maximumRequests){
+                            browser.waitUntil(() => {
+                                return (this.allAnswerButtons[i].isDisplayed());
+                            }, 20000, 'The individual request buttons were not clickable: ' + browser.getUrl());
+                            this.allAnswerButtons[i].click();
+                            i++;
+                        }
+                        console.log('Sent 5 requests');
+                    } else {
+                        console.log('Sent 0 requests');
+                    }
+                    this.popUpCloseButton.click();
+                    console.log('I\'ve clicked on the close button of the popup');
+                }
+            } else {
+                console.log('Trying again! - The popup was not displayed after clicking on one of the three quesitions');
+                browser.refresh();
+                this.clickRequestButtons3();
             }
         } else {
-            console.log('Trying again!');
+            console.log('Trying again! - The page loaded and the three questions needing answers did not load');
             browser.refresh();
             this.clickRequestButtons3();
         }
+
     }
 
     clickRequestButtons4() {
@@ -170,6 +178,19 @@ class QuoraLogin {
         }
         catch (err) {
             console.log('The popup wasn\'t displayed');
+            return false;
+        }
+    }
+
+    checkIfTheThreeQuestionsAreDisplayed() {
+        try {
+            this.allRequestButtons[0].waitForDisplayed(30000);
+            this.allRequestButtons[1].waitForExist(30000);
+            this.allRequestButtons[1].waitForEnabled(30000);
+            return true;
+        }
+        catch (err) {
+            console.log('The Three Questions that need answers were not displayed wasn\'t displayed');
             return false;
         }
     }
