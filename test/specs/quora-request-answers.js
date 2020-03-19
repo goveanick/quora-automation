afaa92a5fc1423ef28f58d11d8e0f8eefb7ba800
goveanick <goveanicolas@gmail.com>
2020-03-18 23:10:22 -0500

    added the correct number in while loop

--- test/specs/quora-request-answers.js
+++ test/specs/quora-request-answers.js
@@ -13,7 +13,7 @@ describe('navigate to Quora and request answers', function() {
         let i = 0;
         // I'm using 38 as the value here because this will run 4 times a day
         // which means ((38 questions * 5 requests) * 4 times) = 760 requests and 152 questions
-        while(i < 3) {
+        while(i < 38) {
             console.log('@@@@@@@@@@@@@ THIS IS RUN NUMBER: ' + i + ' @@@@@@@@@@@@@@@@@@@@@');
             PartnerHomePageObject.clickOnFirstViewAllSuggestionsLink();
             PartnerPromptsPageObject.clickRequestButtons();
