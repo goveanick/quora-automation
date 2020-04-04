bb31d0f07c52b1389d811aea54bd4a516d07daf3
goveanick <goveanicolas@gmail.com>
2020-04-04 13:08:51 -0500

    fixed the delete spec again, quora changed a selector from a span to a div

--- test/specs/quora-delete-useless-questions.js
+++ test/specs/quora-delete-useless-questions.js
@@ -13,10 +13,10 @@ describe('navigate to Quora partner profile', function() {
         console.log('This is the number of questions displayed by default: ' + numberQuestionOptions);
         let i = 8;
         let numDeleted = 0;
-        while (i < 240) {
+        while (i < 3150) {
             $$('span[class*="IconWrapper"] g[id="overflow"]')[i].scrollIntoView(false);
 
-            if (i < 140) {
+            if (i < 200) {
                 console.log('Question: ' + i);
                 browser.pause(1500);
                 i= i +4;
@@ -24,15 +24,16 @@ describe('navigate to Quora partner profile', function() {
                 // console.log('Im going to start clicking the dots');
                 $$('span[class*="IconWrapper"] g[id="overflow"]')[i] + $$('div[class*="action_bar"] svg')[(i * 5) + 4].click();
                 browser.pause(3000);
+                // console.log($$('div[style*="will-change"] div[class*="qu-cursor"]').length);
                 if ($$('div[style*="will-change"] div[class*="qu-cursor"]').length === 11 && $$('div[style*="will-change"] div[class*="qu-cursor"]')[9].getText() === 'Delete') {
-                    console.log('Deleted Question # ' + i + ': ' + $$('span[class*="TitleText"]')[i].getText());
+                    console.log('Deleted Question # ' + i + ': ' + $$('div[class*="TitleText"]')[i].getText());
                     $$('div[style*="will-change"] div[class*="qu-cursor"]')[9].click();
                     browser.pause(1000);
                     $$('a[id*="submit"]')[0].click();
                     browser.pause(1500);
                     numDeleted++;
                 } else {
-                    console.log('Question: ' + i + ': ' + $$('span[class*="TitleText"]')[i].getText());
+                    console.log('Question: ' + i + ': ' + $$('div[class*="TitleText"]')[i].getText());
                 }
                 i++;
             }
