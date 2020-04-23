59596032b6a09aebd13f1c88ce085137cd532085
goveanick <goveanicolas@gmail.com>
2020-04-23 01:23:11 -0500

    fixed this scripted due to quora update

--- test/specs/quora-last-24-hours-request-answers.js
+++ test/specs/quora-last-24-hours-request-answers.js
@@ -13,22 +13,27 @@ describe('navigate to Quora Partner homepage and request answers on the question
         browser.url('https://www.quora.com/partners?sort_by=day#questions');
 
         let i = 0;
-        while($$('div[class*="earnings_amount"]')[i].getText() != '$0.00') {
+        while($$('div[class*="positive"]')[i].getText() != '$0.00') {
             $$('div[class*="QuestionListItem"] a[class="question_link"]')[i].scrollIntoView(false);
             let questionTitle = $$('div[class*="QuestionListItem"] a[class="question_link"]')[i].getText();
+            console.log(questionTitle.toString().indexOf('bra'));
             $$('a[class*="AskToAnswerModalLink"]')[i].click();
             browser.pause(2000);
-            let j = 0;
-            if ($$('div[class*="ClickWrapper"] span[class="q-inlineBlock qu-verticalAlign--text-bottom"]').length > 5) {
-                while (j < 5) {
-                    $$('div[class*="ClickWrapper"] span[class="q-inlineBlock qu-verticalAlign--text-bottom"]')[j].scrollIntoView(false);
-                    $$('div[class*="ClickWrapper"] span[class="q-inlineBlock qu-verticalAlign--text-bottom"]')[j].click();
+            let j = 1;
+            if ($$('div[class*="click-wrapper"] span[class="q-inlineBlock qu-verticalAlign--text-bottom"]').length > 6 && questionTitle.toString().indexOf('bra') === -1) {
+                while (j < 6) {
+                    $$('div[class*="click-wrapper"] span[class="q-inlineBlock qu-verticalAlign--text-bottom"]')[j].scrollIntoView(false);
+                    browser.pause(1000);
+                    $$('div[class*="click-wrapper"] span[class="q-inlineBlock qu-verticalAlign--text-bottom"]')[j].click();
                     browser.pause(2000);
                     j++;
                 }
                 console.log('Sent requests for question: ' + questionTitle);
             } else {
-                console.log('Not enough people to send requests for question: ' + questionTitle)
+                console.log('Not enough people to send requests for question: ' + questionTitle);
+                if (questionTitle.toString().indexOf('bra') != -1) {
+                    console.log('skipped this dumb question');
+                }
             }
             $('span[class*="ButtonPill"]').click();
             browser.pause(2000);
